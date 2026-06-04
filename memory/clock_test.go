2c9f9bc47f1e8f55db409d90231e7da501fa48e0
liyihan <clazs@qq.com>
2026-06-04 18:35:30 +0800

    test: cover leaky bucket injected clock

--- memory/clock_test.go
+++ memory/clock_test.go
@@ -54,6 +54,38 @@ func TestTokenBucket_UsesInjectedClock(t *testing.T) {
 	}
 }
 
+func TestLeakyBucket_UsesInjectedClock(t *testing.T) {
+	ctx := context.Background()
+	clock := newFakeClock(time.Unix(0, 0))
+	limiter := NewLeakyBucket(1, 1, withClock(clock))
+
+	first, err := limiter.Allow(ctx, "user:1")
+	if err != nil {
+		t.Fatalf("unexpected error on first allow: %v", err)
+	}
+	if !first.Allowed {
+		t.Fatal("expected first request to be allowed")
+	}
+
+	second, err := limiter.Allow(ctx, "user:1")
+	if err != nil {
+		t.Fatalf("unexpected error on second allow: %v", err)
+	}
+	if second.Allowed {
+		t.Fatal("expected second request to be denied before clock advances")
+	}
+
+	clock.Advance(time.Second)
+
+	third, err := limiter.Allow(ctx, "user:1")
+	if err != nil {
+		t.Fatalf("unexpected error after clock advance: %v", err)
+	}
+	if !third.Allowed {
+		t.Fatal("expected request to be allowed after clock advance")
+	}
+}
+
 func TestSlidingWindow_UsesInjectedClock(t *testing.T) {
 	ctx := context.Background()
 	clock := newFakeClock(time.Unix(0, 0))
