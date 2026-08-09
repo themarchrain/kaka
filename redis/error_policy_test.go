628c6665f73fcff94ef8a68c30c01466191a676b
liyihan <clazs@qq.com>
2026-08-09 10:52:28 +0800

    test: error policy integration on leaky bucket

--- redis/error_policy_test.go
+++ redis/error_policy_test.go
@@ -48,6 +48,47 @@ func TestTokenBucketFailOpenOnDeadClient(t *testing.T) {
 	}
 }
 
+func TestLeakyBucketFailClosedOnDeadClient(t *testing.T) {
+	client := testClient(t)
+	client.Close()
+
+	var reported error
+	lb := NewLeakyBucket(client, 1, 1,
+		WithOnError(func(err error) { reported = err }))
+
+	res, err := lb.Allow(context.Background(), "k1")
+	if err != nil {
+		t.Fatalf("Allow should not return error on redis failure: %v", err)
+	}
+	if res.Allowed {
+		t.Fatal("fail-closed: should be denied")
+	}
+	if reported == nil {
+		t.Fatal("onError should have been called")
+	}
+}
+
+func TestLeakyBucketFailOpenOnDeadClient(t *testing.T) {
+	client := testClient(t)
+	client.Close()
+
+	var reported error
+	lb := NewLeakyBucket(client, 1, 1,
+		WithErrorPolicy(ErrorFailOpen),
+		WithOnError(func(err error) { reported = err }))
+
+	res, err := lb.Allow(context.Background(), "k1")
+	if err != nil {
+		t.Fatalf("Allow should not return error on redis failure: %v", err)
+	}
+	if !res.Allowed {
+		t.Fatal("fail-open: should be allowed")
+	}
+	if reported == nil {
+		t.Fatal("onError should have been called")
+	}
+}
+
 func TestSlidingWindowFailClosedOnDeadClient(t *testing.T) {
 	client := testClient(t)
 	client.Close()
